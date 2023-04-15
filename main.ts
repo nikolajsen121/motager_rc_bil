a2038e276549ab382c44e333185f35cfe8b4037b
nikolajsen121 <130678378+nikolajsen121@users.noreply.github.com>
2023-04-15 13:54:17 +0200

    Restore '15.4.2023 11.51.17 Opdater main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,37 +1,37 @@
 radio.onReceivedValue(function (name, value) {
     if (name.compare("mgy") == 0) {
-        turn = value
+        backward = value
     }
     if (name.compare("mgx") == 0) {
-        speed = value
+        right = value
     }
-    right_motor = -1 * speed - turn
-    left_motor = -1 * speed + turn
+    right_motor = -1 * backward - right
+    left_motor = -1 * backward + right
 })
 let left_motor = 0
 let right_motor = 0
-let speed = 0
-let turn = 0
+let right = 0
+let backward = 0
+bitbot.ledRainbow()
 radio.setGroup(1)
-basic.showIcon(IconNames.Heart)
-bitbot.ledBrightness(14)
+basic.showIcon(IconNames.House)
 basic.forever(function () {
     if (left_motor > 0) {
-        bitbot.move(BBMotor.Left, BBDirection.Reverse, pins.map(
+        bitbot.move(BBMotor.Left, BBDirection.Forward, pins.map(
         left_motor,
         40,
         1023,
         0,
         100
         ))
-        if (right_motor < 0) {
-            bitbot.move(BBMotor.Right, BBDirection.Reverse, pins.map(
-            right_motor,
-            -40,
-            -1023,
-            0,
-            100
-            ))
-        }
+    }
+    if (right_motor < 0) {
+        bitbot.move(BBMotor.Right, BBDirection.Forward, pins.map(
+        right_motor,
+        -40,
+        -1023,
+        0,
+        100
+        ))
     }
 })
