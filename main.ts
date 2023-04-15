aa37be55766fc09f87faab9e0f5d37066c15dbfa
nikolajsen121 <130678378+nikolajsen121@users.noreply.github.com>
2023-04-15 12:28:34 +0200

    bil kan nu køre frem og bakke

--- main.ts
+++ main.ts
@@ -7,7 +7,14 @@ radio.onReceivedValue(function (name, value) {
     }
     right_motor = -1 * backward - right
     left_motor = -1 * backward + right
+    if (name.compare("button_a") == 0) {
+        direction = 1
+    }
+    if (name.compare("button_b") == 0) {
+        direction = 0
+    }
 })
+let direction = 0
 let left_motor = 0
 let right_motor = 0
 let right = 0
@@ -16,22 +23,43 @@ bitbot.ledRainbow()
 radio.setGroup(1)
 basic.showIcon(IconNames.House)
 basic.forever(function () {
-    if (left_motor > 0) {
-        bitbot.move(BBMotor.Left, BBDirection.Forward, pins.map(
-        left_motor,
-        40,
-        1023,
-        0,
-        100
-        ))
-    }
-    if (right_motor < 0) {
-        bitbot.move(BBMotor.Right, BBDirection.Forward, pins.map(
-        right_motor,
-        -40,
-        -1023,
-        0,
-        100
-        ))
+    if (direction == 0) {
+        if (left_motor > 0) {
+            bitbot.move(BBMotor.Left, BBDirection.Forward, pins.map(
+            left_motor,
+            40,
+            1023,
+            0,
+            100
+            ))
+        }
+        if (right_motor < 0) {
+            bitbot.move(BBMotor.Right, BBDirection.Forward, pins.map(
+            right_motor,
+            -40,
+            -1023,
+            0,
+            100
+            ))
+        }
+    } else {
+        if (left_motor > 0) {
+            bitbot.move(BBMotor.Left, BBDirection.Reverse, pins.map(
+            left_motor,
+            40,
+            1023,
+            0,
+            100
+            ))
+            if (right_motor < 0) {
+                bitbot.move(BBMotor.Right, BBDirection.Reverse, pins.map(
+                right_motor,
+                -40,
+                -1023,
+                0,
+                100
+                ))
+            }
+        }
     }
 })
