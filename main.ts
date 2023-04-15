171b84ee054c3635a5e74f3cbff4805bbb893450
nikolajsen121 <130678378+nikolajsen121@users.noreply.github.com>
2023-04-15 13:52:42 +0200

    wip: test af venstre motor

--- main.ts
+++ main.ts
@@ -1,37 +1,37 @@
 radio.onReceivedValue(function (name, value) {
     if (name.compare("mgy") == 0) {
-        backward = value
+        turn = value
     }
     if (name.compare("mgx") == 0) {
-        right = value
+        speed = value
     }
-    right_motor = backward
-    left_motor = backward
+    right_motor = -1 * speed - turn
+    left_motor = -1 * speed + turn
 })
 let left_motor = 0
 let right_motor = 0
-let right = 0
-let backward = 0
+let speed = 0
+let turn = 0
 radio.setGroup(1)
 basic.showIcon(IconNames.Heart)
 bitbot.ledBrightness(14)
 basic.forever(function () {
     if (left_motor > 0) {
-        bitbot.move(BBMotor.Left, BBDirection.Forward, pins.map(
+        bitbot.move(BBMotor.Left, BBDirection.Reverse, pins.map(
         left_motor,
         40,
         1023,
         0,
         100
         ))
-    }
-    if (right_motor < 0) {
-        bitbot.move(BBMotor.Right, BBDirection.Forward, pins.map(
-        right_motor,
-        -40,
-        -1023,
-        0,
-        100
-        ))
+        if (right_motor < 0) {
+            bitbot.move(BBMotor.Right, BBDirection.Reverse, pins.map(
+            right_motor,
+            -40,
+            -1023,
+            0,
+            100
+            ))
+        }
     }
 })
