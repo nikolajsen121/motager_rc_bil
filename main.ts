58badc1f965727251d3c7630f6da698ae9d1ec55
nikolajsen121 <130678378+nikolajsen121@users.noreply.github.com>
2023-04-15 13:33:13 +0200

    den kan dreje

--- main.ts
+++ main.ts
@@ -5,61 +5,33 @@ radio.onReceivedValue(function (name, value) {
     if (name.compare("mgx") == 0) {
         right = value
     }
-    right_motor = -1 * backward - right
-    left_motor = -1 * backward + right
-    if (name.compare("button_a") == 0) {
-        direction = 1
-    }
-    if (name.compare("button_b") == 0) {
-        direction = 0
-    }
+    right_motor = backward
+    left_motor = backward
 })
-let direction = 0
 let left_motor = 0
 let right_motor = 0
 let right = 0
 let backward = 0
-bitbot.ledRainbow()
 radio.setGroup(1)
-basic.showIcon(IconNames.House)
+basic.showIcon(IconNames.Heart)
+bitbot.ledBrightness(14)
 basic.forever(function () {
-    if (direction == 0) {
-        if (left_motor > 0) {
-            bitbot.move(BBMotor.Left, BBDirection.Forward, pins.map(
-            left_motor,
-            40,
-            1023,
-            0,
-            100
-            ))
-        }
-        if (right_motor < 0) {
-            bitbot.move(BBMotor.Right, BBDirection.Forward, pins.map(
-            right_motor,
-            -40,
-            -1023,
-            0,
-            100
-            ))
-        }
-    } else {
-        if (left_motor > 0) {
-            bitbot.move(BBMotor.Left, BBDirection.Reverse, pins.map(
-            left_motor,
-            40,
-            1023,
-            0,
-            100
-            ))
-            if (right_motor < 0) {
-                bitbot.move(BBMotor.Right, BBDirection.Reverse, pins.map(
-                right_motor,
-                -40,
-                -1023,
-                0,
-                100
-                ))
-            }
-        }
+    if (left_motor > 0) {
+        bitbot.move(BBMotor.Left, BBDirection.Forward, pins.map(
+        left_motor,
+        40,
+        1023,
+        0,
+        100
+        ))
+    }
+    if (right_motor < 0) {
+        bitbot.move(BBMotor.Right, BBDirection.Forward, pins.map(
+        right_motor,
+        -40,
+        -1023,
+        0,
+        100
+        ))
     }
 })
